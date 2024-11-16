40e76b91c3cbbdc6eff12d80e719772987e6b3e8
dhyanauser <dhyanasondagar@gmail.com>
2024-11-16 05:31:40 -0700

    Structural Directive how its work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { StructuralDirectiveComponent } from './components/structural-directive/structural-directive.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DataBindingComponent],
+  imports: [RouterOutlet, DataBindingComponent, StructuralDirectiveComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/structural-directive/structural-directive.component.ts
+++ src/app/components/structural-directive/structural-directive.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-structural-directive',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './structural-directive.component.html',
   styleUrl: './structural-directive.component.css'
 })
 export class StructuralDirectiveComponent {
+  isVisible: boolean = true;
+  items: string[] = ['Apple', 'Banana', 'Orange', 'Mango'];
+  bgColor: string = '';
+
+
+  toggleVisibility() {
+    this.isVisible = !this.isVisible;
+  }
+
 
 }
